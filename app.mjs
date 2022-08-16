4350547ac6c807ae7ea330ad269e6b78ba7854e3
Charles Nyanje <99282156+charlesnyanje@users.noreply.github.com>
2022-08-16 18:13:48 +0000

    Circle-66 phonie

--- app.mjs
+++ app.mjs
@@ -1,17 +1,61 @@
 function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
-  
-    // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
-  
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
+  // Your entire app should not necessarily be coded inside this 
+  // single function (though there's no penalty for that), 
+  // so create and use/call additional functions from here
+
+  // pls remove the below and make some magic in here!
+  const phoneNumber = document.querySelector('#tel');
+  const images = document.querySelectorAll('.image');
+
+  const humburger = document.querySelector('.humburger');
+
+  const navMenu = document.querySelector('.nav-menu');
+
+  const btn = document.querySelector('.btn');
+
+  btn.addEventListener('click', () => {
+    humburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  });
+
+  humburger.addEventListener('click', () => {
+    humburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
+
+  phoneNumber.addEventListener('input', () => {
+
+    if (phoneNumber.value[1] === '9') {
+      images[0].classList.add('active');
+
+    } else {
+      images[0].classList.remove('active');
     }
-  };
-  
-  // ======= DO NOT EDIT ============== //
-  export default startApp;
+
+    if (phoneNumber.value[1] === '8') {
+      images[1].classList.add('active');
+    } else {
+      images[1].classList.remove('active');
+    }
+
+    if (phoneNumber.value[2] === '3') {
+      images[2].classList.add('active');
+    } else {
+      images[2].classList.remove('active');
+    }
+
+    if (phoneNumber.value[2] === '2') {
+      images[3].classList.add('active');
+    } else {
+      images[3].classList.remove('active');
+    }
+
+
+  })
+
+}
+
+
+// ======= DO NOT EDIT ============== //
+export default startApp;
   // ======= EEND DO NOT EDIT ========= //
